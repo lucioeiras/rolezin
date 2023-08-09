645a9481b4e3e147d9bcb9c4c5d7b013c3659e08
Diego Eiras <eiras.lucio@gmail.com>
2023-08-09 18:35:41 -0300

    :tada: Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+
+import express from 'express'
+import cors from 'cors'
+
+import routes from './routes'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(3333, () =>
+  console.log('🚀 Server started at http://localhost:3333'),
+)
